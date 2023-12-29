7942f00056b9f2d5de7e3ca6dc977080d277b5e8
Julius Tens <mail@juliustens.eu>
2023-12-29 16:46:28 +0100

    simplify linter setup

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const server = createServer(api)
 api.use(compression())
 
 const getEnv = name => {
-	const value = (process.env[name] || '').trim()
-	if (!value) throw new Error(`missing env: ${name}`)
-	return value
+  const value = (process.env[name] || '').trim()
+  if (!value) throw new Error(`missing env: ${name}`)
+  return value
 }
 
 api.get('*', async (req, res, next) => {
-	console.log('the example deployment has been called!', new Date())
-	res.end('hello! it works!')
+  console.log('the example deployment has been called!', new Date())
+  res.end('hello! it works!')
 })
 
 const port = getEnv('PORT')
 server.listen(port, error => {
-	if (error) {
-		console.error(error)
-		process.exit(1)
-	}
-	console.log(`Listening on ${port}.`)
+  if (error) {
+    console.error(error)
+    process.exit(1)
+  }
+  console.log(`Listening on ${port}.`)
 })
